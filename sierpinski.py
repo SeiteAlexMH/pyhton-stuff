283f98d9cb7441916be3cfc4c0fb8e1eb346b804
SeiteAlexMH <30759170+SeiteAlexMH@users.noreply.github.com>
2018-08-05 13:15:53 -0500

    Update sierpinski.py

--- sierpinski.py
+++ sierpinski.py
@@ -2,7 +2,7 @@
 #Alexandre Seite 
 import turtle as t 
 def sierpinski(sideLength, levels):    
-  '''a short definition of the sierpinski function'''    
+  #a short definition of the sierpinski function   
   if levels != 0:        
     for n in range(3):            
       sierpinski(sideLength/2,levels-1)            
